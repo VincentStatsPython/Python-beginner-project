9caba3fccef24df84f1c3ab184dd30d04363b30d
Vincent Anyamba <vincentanyamba@gmail.com>
2026-04-26 21:34:13 +0100

    created a new file called vincenthub.py
    This project is a school website that accepts students above 18 years old.
    
    Co-authored-by: Donald <donaldraph.dev@gmail.com>

---
+++ vincenthub.py
@@ -0,0 +1,15 @@
+A collection of my First python projects
+  
+input("\n THIS IS PYTHON WITH VINCENT YOU ARE WELCOME TO UNIZIK APPLICATION PORTAL\n input ok to proceed : ")
+input("Enter your name :")
+input("Enter your course of choice :")
+birth_year = (input("Enter your birth year :"))
+age = 2026 - int(birth_year)
+print(age)
+if age >= 18:
+	print("you are eligible you can proceed")
+elif age <= 18:
+	print("you are ineligible you can't proceed")
+
+This project is a school website that accepts student above 18 years old 
+Correction:
